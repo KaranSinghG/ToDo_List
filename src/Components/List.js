f9f1df515e735a3e49ebfec9e8ba707b46319c4e
Karan Singh <37649078+KaranSinghG@users.noreply.github.com>
2022-05-22 13:10:05 +0530

    Added space in list

--- src/Components/List.js
+++ src/Components/List.js
@@ -6,15 +6,15 @@ function List() {
   return (
     <ul className="ul">
       <li>
-        Item 1<button type="button">Done</button>
+        Item 1 &nbsp;&nbsp;<button type="button">Done</button>
         <button type="button">Remove</button>
       </li>
       <li>
-        Item 2<button type="button">Done</button>
+        Item 2 &nbsp;&nbsp;<button type="button">Done</button>
         <button type="button">Remove</button>
       </li>
       <li>
-        Item 3<button type="button">Done</button>
+        Item 3 &nbsp;&nbsp;<button type="button">Done</button>
         <button type="button">Remove</button>
       </li>
     </ul>
